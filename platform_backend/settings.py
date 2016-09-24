568ba6bb3cbe631f67a51b29c6b6b5afd0cd78a6
Sander <sander@stamkracht.com>
2016-09-24 18:32:54 +0200

    added media files(?)

--- platform_backend/settings.py
+++ platform_backend/settings.py
@@ -12,6 +12,14 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+# For styling etc
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+MEDIA_ROOT = os.path.join(PROJECT_PATH, 'static/media')
+STATIC_ROOT = os.path.join(PROJECT_PATH,'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, 'core/static'),
+)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
