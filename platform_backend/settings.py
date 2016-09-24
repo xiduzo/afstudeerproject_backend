7cb06572415cc7dccab8046ab97c828002cf23f7
Sander <sander@stamkracht.com>
2016-09-24 17:46:07 +0200

    changed database url

--- platform_backend/settings.py
+++ platform_backend/settings.py
@@ -107,13 +107,13 @@ WSGI_APPLICATION = 'platform_backend.wsgi.application'
 # HEROKU
 DATABASES = {
     'default': {
-        dj_database_url.config(default='postgres://hrjffvukjphrbg:lk8gshy9nSH7L0IhL6gRBvqN94@ec2-54-221-226-72.compute-1.amazonaws.com:5432/d4lptoa7krkf8')
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': 'd4lptoa7krkf8',
-        # 'USER': 'hrjffvukjphrbg',
-        # 'PASSWORD': 'lk8gshy9nSH7L0IhL6gRBvqN94',
-        # 'HOST': 'ec2-54-221-226-72.compute-1.amazonaws.com',
-        # 'PORT': '5432',
+        # dj_database_url.config(default='postgres://hrjffvukjphrbg:lk8gshy9nSH7L0IhL6gRBvqN94@ec2-54-221-226-72.compute-1.amazonaws.com:5432/d4lptoa7krkf8')
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd4lptoa7krkf8',
+        'USER': 'hrjffvukjphrbg',
+        'PASSWORD': 'lk8gshy9nSH7L0IhL6gRBvqN94',
+        'HOST': 'ec2-54-221-226-72.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
