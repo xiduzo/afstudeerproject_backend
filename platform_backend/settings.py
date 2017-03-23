7220c57687c39b82b19d057273e96b5bbccd3cde
Sander <mail@sanderboer.nl>
2017-03-23 14:14:18 +0100

    changed some shit, maybe this will help for the static files

--- platform_backend/settings.py
+++ platform_backend/settings.py
@@ -17,7 +17,7 @@ PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 MEDIA_ROOT = os.path.join(PROJECT_PATH, 'static/media')
 STATIC_ROOT = os.path.join(PROJECT_PATH,'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_PATH, 'core/static'),
+    os.path.join(PROJECT_PATH, 'static'),
 )
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
