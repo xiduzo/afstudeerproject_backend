a280ef4800b0b931e50840ab64a8425044fa6f09
Sander <mail@sanderboer.nl>
2017-03-23 13:50:16 +0100

    revert back to /static/

--- platform_backend/settings.py
+++ platform_backend/settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = 'www.sanderboer.nl/school/django/'
+STATIC_URL = '/static/'
 
 # Cors settings
 # https://github.com/ottoyiu/django-cors-headers
